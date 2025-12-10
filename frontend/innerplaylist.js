9321994eb1c71fc75d7a4785316452e58644a9b9
samarthFTR <146484229+samarthFTR@users.noreply.github.com>
2025-12-10 23:32:37 +0530

    Update API_BASE URL for production environment

--- frontend/innerplaylist.js
+++ frontend/innerplaylist.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api" || "https://spotify-player-clone-a9rr.vercel.app";
+const API_BASE = "https://spotify-player-clone-a9rr.vercel.app/api" || "http://localhost:5000/api";
 const playlistId = new URLSearchParams(window.location.search).get("id");
 
 const playlistNameEl = document.getElementById("playlistName");
