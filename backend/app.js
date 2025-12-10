df209046fccdbccd54f47f2c27b4d3694b274214
samarthFTR <146484229+samarthFTR@users.noreply.github.com>
2025-12-10 23:43:43 +0530

    Add default 404 handler for undefined routes

--- backend/app.js
+++ backend/app.js
@@ -24,4 +24,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Spotify-Player-Clone API is running" });
 });
 
+// Default 404 for undefined routes
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
 export default app;
