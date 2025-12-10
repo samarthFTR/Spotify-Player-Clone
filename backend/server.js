a321ab85f146db013065b30deb0b825137ecad6d
samarthFTR <146484229+samarthFTR@users.noreply.github.com>
2025-12-10 23:24:34 +0530

    Refactor server.js to import app from app.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,4 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import songRoutes from "./routes/song.routes.js";
-import likedRoutes from "./routes/liked.routes.js";
-import playlistRoutes from "./routes/playlists.routes.js";
-
-
-dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-connectDB();
-
-app.use("/api/songs", songRoutes);
-app.use("/api/liked", likedRoutes);
-app.use("/api/playlists", playlistRoutes);
-app.use("/api/songs", songRoutes);
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🎧 Server running on port ${PORT}`));
